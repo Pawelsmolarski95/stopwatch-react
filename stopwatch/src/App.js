032520b295e83890732b67aceec4e0a0432ad8de
Pawelsmolarski95 <109138583+Pawelsmolarski95@users.noreply.github.com>
2022-10-18 20:25:24 +0200

    app template

---
+++ stopwatch/src/App.js
@@ -0,0 +1,10 @@
+import Stopwatch from './components/Stopwatch.js/Stopwatch'
+
+
+const App = () => {
+  return(
+    <Stopwatch/>
+  )
+};
+
+export default App;
---
+++ stopwatch/src/components/Stopwatch.js
@@ -0,0 +1,1 @@
+import styles from './Stopwatch.module.scss';
